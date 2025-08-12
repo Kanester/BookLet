5e661a5c8ed4f0ff0f45d73ed8ef131be3ab47df
Xiöv <pampagkieven091@gmail.com>
2025-08-12 18:48:18 +0800

    vercel analytics

---
+++ src/hooks.client.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
+if (dev) eruda.import('eruda').then(e => e.init());
